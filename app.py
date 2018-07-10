be675b75f03a77dd9301f257d55adca22d0065c8
Sudeep <sudeep@getvymo.com>
2018-07-10 15:49:59 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health():
+    """
+    Does Health Check
+    """
+    response_object = {"success": "1","project":"ritiksparser"}
+    response = jsonify(response_object)
+    response.status_code = 200
+    print('Health OK')
+    return response
+
+if __name__ == '__main__':
+    app.run()
